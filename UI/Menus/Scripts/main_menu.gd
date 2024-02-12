a758d55e883811e34dffad468f9835471e0053a7
Robotech83 <volkswagenjetta@protonmail.com>
2024-02-12 10:14:17 -0800

    fix bugs

--- UI/Menus/Scripts/main_menu.gd
+++ UI/Menus/Scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://World/tutorial.tscn")
+	get_tree().change_scene_to_file("res://World/Labyrinth.tscn")
 
 
 func _on_options_button_pressed():
@@ -17,3 +17,8 @@ func _on_credits_button_pressed():
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+
+
+func _on_tutorial_button_pressed():
+	get_tree().change_scene_to_file("res://World/tutorial.tscn")
